971851f4f44871266752eaeaeffdc4a53f4bf0ce
Matic Kukovec <kukovecmatic@hotmail.com>
2018-06-16 11:57:25 +0200

    updated the commenting example to smart indent at the minimal indent level of each selection and added merging of same line selections

--- examples/commenting.py
+++ examples/commenting.py
@@ -35,11 +35,14 @@ class MyCommentingEditor(PyQt5.Qsci.QsciScintilla):
         selections = self.get_selections()
         if selections == None:
             return
+        # Merge overlapping selections
+        while self.merge_test(selections) == True:
+            selections = self.merge_selections(selections)
         # Start the undo action that can undo all commenting at once
         self.beginUndoAction()
         # Loop over selections and comment them
         for i, sel in enumerate(selections):
-            if self.text(sel[0]).strip().startswith(self.comment_string):
+            if self.text(sel[0]).lstrip().startswith(self.comment_string):
                 self.set_commenting(sel[0], sel[1], self._uncomment)
             else:
                 self.set_commenting(sel[0], sel[1], self._comment)
@@ -52,6 +55,7 @@ class MyCommentingEditor(PyQt5.Qsci.QsciScintilla):
                 self.SendScintilla(self.SCI_SETSELECTION, start_index, end_index)
             else:
                 self.SendScintilla(self.SCI_ADDSELECTION, start_index, end_index)
+        # Set the end of the undo action
         self.endUndoAction()
     
     def get_selections(self):
@@ -73,6 +77,56 @@ class MyCommentingEditor(PyQt5.Qsci.QsciScintilla):
         # Return selection list
         return selections
     
+    def merge_test(self, selections):
+        """
+        Test if merging of selections is needed
+        """
+        for i in range(1, len(selections)):
+            # Get the line numbers
+            previous_start_line = selections[i-1][0]
+            previous_end_line = selections[i-1][1]
+            current_start_line = selections[i][0]
+            current_end_line = selections[i][1]
+            if previous_end_line == current_start_line:
+                return True
+        # Merging is not needed
+        return False
+    
+    def merge_selections(self, selections):
+        """
+        This function merges selections with overlapping lines
+        """
+        # Test if merging is required
+        if len(selections) < 2:
+            return selections
+        merged_selections = []
+        skip_flag = False
+        for i in range(1, len(selections)):
+            # Get the line numbers
+            previous_start_line = selections[i-1][0]
+            previous_end_line = selections[i-1][1]
+            current_start_line = selections[i][0]
+            current_end_line = selections[i][1]
+            # Test for merge
+            if previous_end_line == current_start_line and skip_flag == False:
+                merged_selections.append(
+                    (previous_start_line, current_end_line)
+                )
+                skip_flag = True
+            else:
+                if skip_flag == False:
+                    merged_selections.append(
+                        (previous_start_line, previous_end_line)
+                    )
+                skip_flag = False
+                # Add the last selection only if it was not merged
+                if i == (len(selections) - 1):
+                    merged_selections.append(
+                        (current_start_line, current_end_line)
+                    )
+        # Return the merged selections
+        return merged_selections
+    
     def set_commenting(self, arg_from_line, arg_to_line, func):
         # Get the cursor information
         from_line = arg_from_line
@@ -85,21 +139,27 @@ class MyCommentingEditor(PyQt5.Qsci.QsciScintilla):
         # Get the selected text and split it into lines
         selected_text = self.selectedText()
         selected_list = selected_text.split("\n")
+        # Find the smallest indent level
+        indent_levels = []
+        for line in selected_list:
+            indent_levels.append(len(line) - len(line.lstrip()))
+        min_indent_level = min(indent_levels)
         # Add the commenting character to every line
         for i, line in enumerate(selected_list):
-            selected_list[i] = func(line)
+            selected_list[i] = func(line, min_indent_level)
         # Replace the whole selected text with the merged lines
         # containing the commenting characters
         replace_text = self.line_ending.join(selected_list)
         self.replaceSelectedText(replace_text)
     
-    def _comment(self, line):
+    def _comment(self, line, indent_level):
         if line.strip() != "":
-            return self.comment_string + line
+#            return self.comment_string + line
+            return line[:indent_level] + self.comment_string + line[indent_level:]
         else:
             return line
     
-    def _uncomment(self, line):
+    def _uncomment(self, line, indent_level):
         if line.strip().startswith(self.comment_string):
             return line.replace(self.comment_string, "", 1)
         else:
